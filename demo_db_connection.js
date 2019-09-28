55c7498d6fdf47f0c6c4fdac472facdb60a0cc30
Daria Lazovskaya <32599716+odasco@users.noreply.github.com>
2019-09-29 00:17:25 +0300

    getting patients, adding

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
-    port: 3000,
+    port: 3306,
     user: "root",
     password: "root"
 });
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const mysql = require('mysql');
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+let patients = [];
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, 'public/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '.jpg') //Appending .jpg
@@ -13,46 +21,61 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const app = express();
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
+connection.query('SELECT * FROM patient', (err,rows) => {
+    if(err) throw err;
+    patients = rows;
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-const players = [
-    {
-        id: 1,
-        name: 'New Dasha'
-    }
-];
-
 app.get('/', (req, res) => {
     res.send('hello api');
 });
 
-app.get('/players', (req, res) => {
+app.get('/patient', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    res.send(players);
+    res.send(patients);
 });
 
-app.get('/players/:id', (req, res) => {
+app.get('/patient/:id', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    const player = players.find((player) => {
-        return player.id === Number(req.params.id);
+    const patient = patients.find((patient) => {
+        return patient.id === Number(req.params.id);
     });
-    res.send(player);
+    res.send(patient);
 });
 
-app.post('/players', upload.single('file'), function (req, res, next) {
-    const player = {
-        id: Date.now(),
-        name: req.body.name
+app.post('/patient', upload.single('file'), function (req, res, next) {
+    const patient = {
+        id: +Date.now().toString().split('').slice(6).join(''),
+        name: req.body.name,
+        photo: req.file.filename
     };
-    players.push(player);
+    patients.push(patient);
+    connection.query(`INSERT INTO patient VALUES (${patient.id}, '${patient.name}', '${patient.photo}')`, (err,rows) => {
+        if(err) throw err;
+    });
+    res.send(patient);
     res.sendStatus(200);
 });
+
+
 
 app.listen(3012, () => {
     console.log(123);
