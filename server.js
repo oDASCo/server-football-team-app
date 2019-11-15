c0f542ee2e7c7d3b7ed3ea7d5c706fd5dad400e6
Daria Lazovskaya <32599716+odasco@users.noreply.github.com>
2019-11-15 13:22:49 +0300

    last 15.11.19

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const multer = require('multer');
 const mysql = require('mysql');
 
 const app = express();
-
-
 app.use(express.static('public'));
 
 let patients = [];
@@ -22,7 +20,6 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + '.jpg') //Appending .jpg
     }
 });
-
 const upload = multer({storage: storage});
 
 const connection = mysql.createConnection({
@@ -74,16 +71,22 @@ app.post('/patient', upload.single('file'), function (req, res, next) {
         photo: req.file.filename
     };
     patients.push(patient);
-    connection.query(`INSERT INTO patient VALUES (${patient.id}, '${patient.name}', '${patient.photo}')`, (err, rows) => {
-        if (err) throw err;
-    });
+    connection.query(`INSERT INTO patient VALUES (${patient.id}, '${patient.name}', '${patient.photo}')`,
+        (err, rows) => {
+            if (err) throw err;
+        });
+    connection.query(`INSERT INTO generalinfo VALUES (${patient.id}, '${'2000-01-01'}',
+     '${0}', '${0}', '${0}', '${0}', '${patient.id}')`,
+        (err, rows) => {
+            if (err) throw err;
+        });
     res.send(patient);
 });
 
 app.put('/patient', upload.single('file'), function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    let photo = req.file?req.file.filename:req.body.photoName;
+    let photo = req.file ? req.file.filename : req.body.photoName;
     const patient = {
         id: req.body.id,
         name: req.body.name,
@@ -102,6 +105,20 @@ app.put('/patient', upload.single('file'), function (req, res, next) {
 app.delete('/patient/:id', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    connection.query(`DELETE FROM mydb.fluorography WHERE GeneralInfo_id = ${req.params.id}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.surgicalintervention WHERE GeneralInfo_id = ${req.params.id}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.vaccinationstatus WHERE GeneralInfo_id = ${req.params.id}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.generalinfo WHERE patientId = ${req.params.id}`, (err) => {
+        if (err) throw err;
+    });
+    deleteIllness(req.params.id);
+    deleteMedExam(req.params.id);
     connection.query(`DELETE FROM mydb.patient WHERE id = ${req.params.id}`, (err) => {
         if (err) throw err;
     });
@@ -123,22 +140,24 @@ app.get('/generalInfo/:patientId', (req, res) => {
     }, 1000);
 });
 
-
-app.put('/generalInfo', function (req, res, next) {
+app.put('/generalInfo', async function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    connection.query(`UPDATE mydb.generalinfo SET birthday="${"2000-09-09"}", 
+        try {
+              await connection.query(`UPDATE mydb.generalinfo SET birthday="${req.body.date}", 
     weight="${req.body.weight}",
      height="${req.body.height}",
      arterialPressure="${req.body.arterialPressure}",
      bloodType="${req.body.bloodType}"
      WHERE id=${req.body.id}`, (err, rows) => {
-        if (err) throw err;
-    });
-
-    res.send('OK');
-});
-
+                if (err) throw err;
+            });
+            res.sendStatus(200).send('OK');
+        } catch (err) {
+            res.sendStatus(500);
+        }
+    }
+);
 
 app.get('/fluorography/:generalInfoId', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -152,7 +171,7 @@ app.get('/fluorography/:generalInfoId', (req, res) => {
     }, 1000);
 });
 
-app.post('/fluorography',  function (req, res, next) {
+app.post('/fluorography', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     connection.query(`INSERT INTO mydb.fluorography VALUES (${req.body.id},
@@ -163,6 +182,7 @@ app.post('/fluorography',  function (req, res, next) {
     });
     res.send("OK");
 });
+
 app.delete('/fluorography/:id', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
@@ -172,8 +192,7 @@ app.delete('/fluorography/:id', function (req, res) {
     res.send("OK");
 });
 
-
-app.post('/surgicalintervention',  function (req, res, next) {
+app.post('/surgicalintervention', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     connection.query(`INSERT INTO mydb.surgicalintervention VALUES (${req.body.id},
@@ -186,6 +205,7 @@ app.post('/surgicalintervention',  function (req, res, next) {
     });
     res.send("OK");
 });
+
 app.delete('/surgicalintervention/:id', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
@@ -194,7 +214,8 @@ app.delete('/surgicalintervention/:id', function (req, res) {
     });
     res.send("OK");
 });
-app.post('/vaccinationstatus',  function (req, res, next) {
+
+app.post('/vaccinationstatus', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     connection.query(`INSERT INTO mydb.vaccinationstatus VALUES (${req.body.id},
@@ -205,6 +226,7 @@ app.post('/vaccinationstatus',  function (req, res, next) {
     });
     res.send("OK");
 });
+
 app.delete('/vaccinationstatus/:id', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
@@ -238,6 +260,305 @@ app.get('/vaccinationstatus/:generalInfoId', (req, res) => {
     }, 1000);
 });
 
+let medicalExamination = [];
+
+app.get('/medicalExamination/:patientId', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    connection.query(`SELECT * FROM mydb.medicalexamination WHERE patientId = ${req.params.patientId}`,
+        (err, rows) => {
+            if (err) throw err;
+            medicalExamination = rows;
+            medicalExamination.forEach(row => {
+                getRows(row.id, 'bloodchemistryanalysis', function (data) {
+                    row.bloodchemistryanalysis = data;
+                });
+                getRows(row.id, 'doctordiagnosis', function (data) {
+                    row.doctordiagnosis = data;
+                });
+                getRows(row.id, 'electrocardiogram', function (data) {
+                    row.electrocardiogram = data;
+                });
+                getRows(row.id, 'generalbloodanalysis', function (data) {
+                    row.generalbloodanalysis = data;
+                });
+                getRows(row.id, 'generalurineanalysis', function (data) {
+                    row.generalurineanalysis = data;
+                });
+                getRows(row.id, 'heartultrasound', function (data) {
+                    row.heartultrasound = data;
+                });
+            });
+        });
+
+    function getRows(medId, rowName, callback) {
+        if (rowName === 'generalurineanalysis') {
+            connection.query(`SELECT * FROM mydb.${rowName} WHERE medicalExamnationId = ${medId}`,
+                (err, rows) => {
+                    if (err) throw err;
+                    callback(rows);
+                });
+        } else {
+            connection.query(`SELECT * FROM mydb.${rowName} WHERE medicalExaminationId = ${medId}`,
+                (err, rows) => {
+                    if (err) throw err;
+                    callback(rows);
+                });
+        }
+
+    }
+
+    setTimeout(() => {
+        res.send(medicalExamination);
+    }, 1000);
+});
+let fields = [{name: 'bloodchemistryanalysis'},
+    {name: 'electrocardiogram'},
+    {name: 'generalbloodanalysis'},
+    {name: 'generalurineanalysis'},
+    {name: 'heartultrasound'}
+];
+app.post('/medicalExamination', upload.fields(fields), function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    const medicalExamination = {
+        id: +Date.now().toString().split('').slice(6).join(''),
+        patientId: req.body.patientId,
+        date: req.body.date,
+        allowance: req.body.allowance,
+        doctordiagnosis: JSON.parse(req.body.doctordiagnosis)
+    };
+    connection.query(`INSERT INTO mydb.medicalexamination 
+    (id, date, allowance, patientId) VALUES ('${medicalExamination.id}', '${medicalExamination.date}', 
+    '${medicalExamination.allowance}', '${medicalExamination.patientId}');`, (err, rows) => {
+        if (err) throw err;
+    });
+    medicalExamination.doctordiagnosis.forEach(item => {
+        connection.query(`INSERT INTO mydb.doctordiagnosis 
+        (id, doctorName, diagnosis, medicalExaminationId) VALUES ('${item.id}', '${item.doctorName}', 
+        '${item.diagnosis}', '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    });
+    const files = {};
+    if (req.files.bloodchemistryanalysis !== undefined) {
+        files.bloodchemistryanalysis = {
+            bloodchemistryanalysis: req.files.bloodchemistryanalysis[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.bloodchemistryanalysis 
+        (id, info, medicalExaminationId) VALUES ('${files.bloodchemistryanalysis.id}',
+         '${files.bloodchemistryanalysis.bloodchemistryanalysis}',
+         '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.electrocardiogram !== undefined) {
+        files.electrocardiogram = {
+            electrocardiogram: req.files.electrocardiogram[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.electrocardiogram 
+        (id, info, medicalExaminationId) VALUES ('${files.electrocardiogram.id}', 
+        '${files.electrocardiogram.electrocardiogram}',
+         '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.generalbloodanalysis !== undefined) {
+        files.generalbloodanalysis = {
+            generalbloodanalysis: req.files.generalbloodanalysis[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.generalbloodanalysis 
+        (id, info, medicalExaminationId) VALUES ('${files.generalbloodanalysis.id}', 
+        '${files.generalbloodanalysis.generalbloodanalysis}',
+         '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.generalurineanalysis !== undefined) {
+        files.generalurineanalysis = {
+            generalurineanalysis: req.files.generalurineanalysis[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.generalurineanalysis 
+        (id, info, medicalExamnationId) VALUES ('${files.generalurineanalysis.id}', 
+        '${files.generalurineanalysis.generalurineanalysis}',
+         '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.heartultrasound !== undefined) {
+        files.heartultrasound = {
+            heartultrasound: req.files.heartultrasound[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.heartultrasound 
+        (id, info, medicalExaminationId) VALUES ('${files.heartultrasound.id}', 
+        '${files.heartultrasound.heartultrasound}',
+         '${medicalExamination.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+
+    res.send(medicalExamination);
+});
+
+function deleteMedExam(patientId) {
+    connection.query(`DELETE FROM mydb.heartultrasound WHERE medicalExaminationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.generalurineanalysis WHERE medicalExamnationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.generalbloodanalysis WHERE medicalExaminationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.electrocardiogram WHERE medicalExaminationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.doctordiagnosis WHERE medicalExaminationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.bloodchemistryanalysis WHERE medicalExaminationId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+
+    connection.query(`DELETE FROM mydb.medicalexamination WHERE id = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+}
+
+app.delete('/medicalExamination/:id', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    deleteMedExam(req.params.id);
+    res.send(req.params.id);
+});
+
+let injuriesDiseases = [];
+
+app.get('/injuriesDiseases/:patientId', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    connection.query(`SELECT * FROM mydb.injuriesdiseases WHERE patientId = ${req.params.patientId}`, (err, rows) => {
+        if (err) throw err;
+        injuriesDiseases = rows;
+        injuriesDiseases.forEach(row => {
+            getIlRows(row.id, 'mri', function (data) {
+                row.mri = data;
+            });
+            getIlRows(row.id, 'radiographies', function (data) {
+                row.radiographies = data;
+            });
+            getIlRows(row.id, 'commonultrasound', function (data) {
+                row.commonultrasound = data;
+            });
+        });
+    });
+
+    function getIlRows(medId, rowName, callback) {
+        connection.query(`SELECT * FROM mydb.${rowName} WHERE injurieDiseaseId = ${medId}`,
+            (err, rows) => {
+                if (err) throw err;
+                callback(rows);
+            });
+    }
+
+    setTimeout(() => {
+        res.send(injuriesDiseases);
+    }, 1000);
+});
+
+let fieldsIllness = [{name: 'mri'},
+    {name: 'radiographies'},
+    {name: 'commonultrasound'}
+];
+
+app.post('/injuriesDiseases', upload.fields(fieldsIllness), function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    const illness = {
+        id: +Date.now().toString().split('').slice(6).join(''),
+        illnessStartDate: req.body.illnessStartDate,
+        illnessEndDate: req.body.illnessEndDate,
+        diagnosis: req.body.diagnosis,
+        drugTherapy: req.body.drugTherapy,
+        physiotherapyTreatment: req.body.physiotherapyTreatment,
+        other: req.body.other,
+        patientId: req.body.patientId
+    };
+
+    connection.query(`INSERT INTO mydb.injuriesdiseases
+    (id, date, releasedInMainGroup, disabilityCountDays, diagnosis, 
+    drugTherapy, physiopherapy, other, disabilityTypeId, patientId) 
+    VALUES ('${illness.id}', '${illness.illnessStartDate}', '${5}', '${5}', 
+    '${illness.diagnosis}', '${illness.drugTherapy}', '${illness.physiotherapyTreatment}', 
+    '${illness.other}', '${1}', '${illness.patientId}');`, (err, rows) => {
+        if (err) throw err;
+    });
+
+    const files = {};
+    if (req.files.mri !== undefined) {
+        files.mri = {
+            mri: req.files.mri[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.mri
+        (id, info, injurieDiseaseId) VALUES ('${files.mri.id}', '${files.mri.mri}',
+         '${illness.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.radiographies !== undefined) {
+        files.radiographies = {
+            radiographies: req.files.radiographies[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.radiographies
+        (id, info, injurieDiseaseId) VALUES ('${files.radiographies.id}', '${files.radiographies.radiographies}',
+         '${illness.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    if (req.files.commonultrasound !== undefined) {
+        files.commonultrasound = {
+            commonultrasound: req.files.commonultrasound[0].filename,
+            id: +Date.now().toString().split('').slice(6).join('')
+        };
+        connection.query(`INSERT INTO mydb.commonultrasound
+        (id, info, injurieDiseaseId) VALUES ('${files.commonultrasound.id}', '${files.commonultrasound.commonultrasound}',
+         '${illness.id}');`, (err, rows) => {
+            if (err) throw err;
+        });
+    }
+    console.log(files);
+    res.send(illness);
+});
+
+function deleteIllness(patientId) {
+    connection.query(`DELETE FROM mydb.commonultrasound WHERE injurieDiseaseId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.mri WHERE injurieDiseaseId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.radiographies WHERE injurieDiseaseId = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+    connection.query(`DELETE FROM mydb.injuriesdiseases WHERE id = ${patientId}`, (err) => {
+        if (err) throw err;
+    });
+}
+
+app.delete('/injuriesDiseases/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    deleteIllness(req.params.id);
+    res.send(req.params.id);
+});
+
 app.listen(3012, () => {
-    console.log(123);
+    console.log("Server started");
 });
